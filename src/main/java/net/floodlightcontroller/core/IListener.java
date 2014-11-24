ebd50725fdd7eb3329bd1c50566788dadc1a1751
vonzhou <vonzhou@163.com>
2014-11-24 14:40:45 +0800

    消息分发

--- src/main/java/net/floodlightcontroller/core/IListener.java
+++ src/main/java/net/floodlightcontroller/core/IListener.java
@@ -1,19 +1,3 @@
-/**
-*    Copyright 2011, Big Switch Networks, Inc. 
-*    Originally created by David Erickson, Stanford University
-* 
-*    Licensed under the Apache License, Version 2.0 (the "License"); you may
-*    not use this file except in compliance with the License. You may obtain
-*    a copy of the License at
-*
-*         http://www.apache.org/licenses/LICENSE-2.0
-*
-*    Unless required by applicable law or agreed to in writing, software
-*    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-*    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-*    License for the specific language governing permissions and limitations
-*    under the License.
-**/
 
 package net.floodlightcontroller.core;
 
@@ -24,11 +8,11 @@ public interface IListener<T> {
     
     /**
      * The name assigned to this listener
-     * @return
      */
     public String getName();
 
     /**
+     * 名为name的module在处理这个消息的时候，要在这个module之前
      * Check if the module called name is a callback ordering prerequisite
      * for this module.  In other words, if this function returns true for 
      * the given name, then this message listener will be called after that
@@ -40,6 +24,7 @@ public interface IListener<T> {
     public boolean isCallbackOrderingPrereq(T type, String name);
 
     /**
+     * 在处理type消息的时候，name_module 要在这个模块之后
      * Check if the module called name is a callback ordering post-requisite
      * for this module.  In other words, if this function returns true for 
      * the given name, then this message listener will be called before that

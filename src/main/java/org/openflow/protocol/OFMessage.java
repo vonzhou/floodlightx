faeaddbb5b51dc131575f0f5428ce6277de06895
vonzhou <vonzhou@163.com>
2014-12-22 19:09:12 +0800

    of_config消息

--- src/main/java/org/openflow/protocol/OFMessage.java
+++ src/main/java/org/openflow/protocol/OFMessage.java
@@ -1,20 +1,3 @@
-/**
-*    Copyright (c) 2008 The Board of Trustees of The Leland Stanford Junior
-*    University
-* 
-*    Licensed under the Apache License, Version 2.0 (the "License"); you may
-*    not use this file except in compliance with the License. You may obtain
-*    a copy of the License at
-*
-*         http://www.apache.org/licenses/LICENSE-2.0
-*
-*    Unless required by applicable law or agreed to in writing, software
-*    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-*    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-*    License for the specific language governing permissions and limitations
-*    under the License.
-**/
-
 package org.openflow.protocol;
 
 import java.text.DateFormat;
@@ -36,6 +19,7 @@ import org.openflow.util.U8;
 /**
  * The base class for all OpenFlow protocol messages. This class contains the
  * equivalent of the ofp_header which is present in all OpenFlow messages.
+ * openflowœ˚œ¢Õ∑
  *
  * @author David Erickson (daviderickson@cs.stanford.edu) - Feb 3, 2010
  * @author Rob Sherwood (rob.sherwood@stanford.edu) - Feb 3, 2010
--- src/main/java/org/openflow/protocol/OFSwitchConfig.java
+++ src/main/java/org/openflow/protocol/OFSwitchConfig.java
@@ -1,20 +1,3 @@
-/**
-*    Copyright (c) 2008 The Board of Trustees of The Leland Stanford Junior
-*    University
-* 
-*    Licensed under the Apache License, Version 2.0 (the "License"); you may
-*    not use this file except in compliance with the License. You may obtain
-*    a copy of the License at
-*
-*         http://www.apache.org/licenses/LICENSE-2.0
-*
-*    Unless required by applicable law or agreed to in writing, software
-*    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-*    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-*    License for the specific language governing permissions and limitations
-*    under the License.
-**/
-
 package org.openflow.protocol;
 
 
@@ -22,7 +5,7 @@ import org.jboss.netty.buffer.ChannelBuffer;
 
 /**
  * Base class representing ofp_switch_config based messages
- * @author David Erickson (daviderickson@cs.stanford.edu)
+ * 代表Switch的配置信息，set config 和 get config reply的时候需要
  */
 public abstract class OFSwitchConfig extends OFMessage {
     public static int MINIMUM_LENGTH = 12;

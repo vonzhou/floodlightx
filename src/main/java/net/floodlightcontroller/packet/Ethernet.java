7067fcd59b848893ff7af0ac944f4f30a734f24a
vonzhou <vonzhou@163.com>
2014-12-23 17:11:28 +0800

    Ethernet

--- src/main/java/net/floodlightcontroller/packet/Ethernet.java
+++ src/main/java/net/floodlightcontroller/packet/Ethernet.java
@@ -1,20 +1,3 @@
-/**
-*    Copyright 2011, Big Switch Networks, Inc. 
-*    Originally created by David Erickson, Stanford University
-* 
-*    Licensed under the Apache License, Version 2.0 (the "License"); you may
-*    not use this file except in compliance with the License. You may obtain
-*    a copy of the License at
-*
-*         http://www.apache.org/licenses/LICENSE-2.0
-*
-*    Unless required by applicable law or agreed to in writing, software
-*    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-*    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-*    License for the specific language governing permissions and limitations
-*    under the License.
-**/
-
 package net.floodlightcontroller.packet;
 
 import java.nio.ByteBuffer;
@@ -31,6 +14,8 @@ import org.openflow.util.HexString;
  */
 public class Ethernet extends BasePacket {
     private static String HEXES = "0123456789ABCDEF";
+    
+    // 各种ether type
     public static final short TYPE_ARP = 0x0806;
     public static final short TYPE_IPv4 = 0x0800;
     public static final short TYPE_LLDP = (short) 0x88cc;
@@ -95,6 +80,7 @@ public class Ethernet extends BasePacket {
     }
 
     /**
+     * 得到源MAC地址的字节序  ，通常由此得到十六进制的字符串形式 
      * @return the source MACAddress as a byte array
      */
     public byte[] getSourceMACAddress() {

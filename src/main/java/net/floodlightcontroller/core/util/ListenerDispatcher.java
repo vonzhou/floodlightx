60fbb669dda1e5a968043a1be8c706f86dd72c55
vonzhou <vonzhou@163.com>
2014-12-23 10:05:34 +0800

    ListenerDispather

--- src/main/java/net/floodlightcontroller/core/util/ListenerDispatcher.java
+++ src/main/java/net/floodlightcontroller/core/util/ListenerDispatcher.java
@@ -1,20 +1,3 @@
-/**
-*    Copyright 2011, Big Switch Networks, Inc. 
-*    Originally created by David Erickson, Stanford University
-* 
-*    Licensed under the Apache License, Version 2.0 (the "License"); you may
-*    not use this file except in compliance with the License. You may obtain
-*    a copy of the License at
-*
-*         http://www.apache.org/licenses/LICENSE-2.0
-*
-*    Unless required by applicable law or agreed to in writing, software
-*    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-*    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-*    License for the specific language governing permissions and limitations
-*    under the License.
-**/
-
 package net.floodlightcontroller.core.util;
 
 import java.util.ArrayList;
@@ -27,10 +10,10 @@ import org.slf4j.LoggerFactory;
 import net.floodlightcontroller.core.IListener;
 
 /**
+ * 保持各个消息的观察者，这个listener 是根据依赖关系排序的！
  * Maintain lists of listeners ordered by dependency.  
  * 
  * @author readams
- *
  */
 public class ListenerDispatcher<U, T extends IListener<U>> {
     protected static Logger logger = LoggerFactory.getLogger(ListenerDispatcher.class);
@@ -56,6 +39,7 @@ public class ListenerDispatcher<U, T extends IListener<U>> {
     }
     
     /**
+     * 给某个消息增加一个listener，并且维护其中的order；
      * Add a listener to the list of listeners
      * @param listener
      */
